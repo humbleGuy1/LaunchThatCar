06fd0360e7590832387e3f7a804e59feb8c771a5
humbleGuy1 <vrudnevskiy@list.ru>
2023-02-01 20:22:55 +0300

    Fix Rotator

--- Assets/_Project/Scripts/Traps/Rotate.cs
+++ Assets/_Project/Scripts/Traps/Rotate.cs
@@ -6,6 +6,7 @@ public class Rotate : MonoBehaviour, ILoopedTrap
     [SerializeField] private float _x;
     [SerializeField] private float _y;
     [SerializeField] private float _z;
+    [SerializeField] private Rigidbody _rigidbody;
 
     [field: SerializeField] public float Duration { get; private set; }
     [field: SerializeField] public AnimationCurve MotionCurve { get; private set; }
@@ -17,7 +18,7 @@ public class Rotate : MonoBehaviour, ILoopedTrap
 
     public void StartLoop()
     {
-        transform.DOLocalRotate(new Vector3(_x, _y, _z), Duration, RotateMode.FastBeyond360).
+        _rigidbody.DORotate(new Vector3(_x, _y, _z), Duration, RotateMode.FastBeyond360).
             SetEase(MotionCurve).SetLoops(-1, LoopType.Restart);
     }
 }
