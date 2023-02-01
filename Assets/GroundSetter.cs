60fc481140e55c85e512d0441dedb19a0e4156d4
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2023-02-01 21:28:03 +0300

    car stright rid fix

---
+++ Assets/GroundSetter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSetter : MonoBehaviour
+{
+    [ContextMenu("asds")]
+    public void Set()
+    {
+        //var grounds = FindObjectsOfType<Ground>();
+
+        //foreach (var ground in grounds)
+        //{
+        //    ground.Property.SetProperty(200, 10);
+        //}
+    }
+}
--- Assets/_Project/Scripts/Car/GroundProperty.cs
+++ Assets/_Project/Scripts/Car/GroundProperty.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GroundProperty
 {
     [field: SerializeField] public float MaxVelocity {get;private set;}
-    [field: SerializeField] public float MaxAngularDrag { get; private set; }
+    [field: SerializeField] public float MaxAngularDrag { get; private set; } = 5f;
     [field: SerializeField] public float MaxFlySpeed { get; private set; } = 50f;
     [field: SerializeField] public float MaxDrag { get; private set; }
     [field: SerializeField] public float MaxGrip { get; private set; } = 100f;
