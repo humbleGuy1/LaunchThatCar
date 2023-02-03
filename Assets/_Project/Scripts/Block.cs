57ed216bd4d3bd465417327c33f586d778c03e4c
humbleGuy1 <vrudnevskiy@list.ru>
2023-02-03 17:07:11 +0300

    Fix block editor

--- Assets/_Project/Scripts/Block.cs
+++ Assets/_Project/Scripts/Block.cs
@@ -1,46 +1,47 @@
+#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
     [SerializeField] private RespawnPoint _respawnPointPrefab;
     [SerializeField] private Ramp _rampPrefab;
-
-    private RespawnPoint _respawnPoint;
-    private Ramp _ramp;
+    [Header("Spawned Objects")]
+    [SerializeField] private RespawnPoint _createdRespawnPoint;
+    [SerializeField] private Ramp _createdRamp;
 
     public void AddRespawnPoint()
     {
-        if (_respawnPoint != null)
+        if (_createdRespawnPoint != null)
             return;
 
         var respawnPoint = Instantiate(_respawnPointPrefab, transform);
         respawnPoint.transform.localPosition = Vector3.up * 3;
-        _respawnPoint = respawnPoint;
+        _createdRespawnPoint = respawnPoint;
+
+        PrefabUtility.RecordPrefabInstancePropertyModifications(this);
     }
 
     public void RemoveRespawnPoint()
     {
-        if (_respawnPoint == null)
-            return;
-
-        DestroyImmediate(_respawnPoint.gameObject);
+        DestroyImmediate(_createdRespawnPoint.gameObject);
     }
 
     public void AddRamp()
     {
-        if (_ramp != null)
+        if (_createdRamp != null)
             return;
 
         var ramp = Instantiate(_rampPrefab, transform);
         ramp.transform.localPosition = new Vector3(0, -4.5f, 51f);
-        _ramp = ramp;
+        _createdRamp = ramp;
+
+        PrefabUtility.RecordPrefabInstancePropertyModifications(this);
     }
 
     public void RemoveRamp()
     {
-        if (_ramp == null)
-            return;
-
-        DestroyImmediate(_ramp.gameObject);
+        DestroyImmediate(_createdRamp.gameObject);
     }
 }
+# endif
